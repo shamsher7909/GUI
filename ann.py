d0219a131f39ed30389721bdb46d264ecafca70a
shamsher7909 <134923810+shamsher7909@users.noreply.github.com>
2023-07-14 12:13:29 +0500

    Update and rename ann4.py to ann.py

---
+++ ann.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Load the CSV file
+data = pd.read_csv('training.csv')
+
+# Extract input variables (X) and response variable (y)
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+# Variable names
+variable_names = list(data.columns[:-1])
+
+# Create the model
+model = Sequential()
+model.add(Dense(8, input_dim=5, activation='relu'))  # Adjust the number of nodes and input_dim based on your preference
+model.add(Dense(1, activation='linear'))  # Use 'linear' activation for regression tasks
+
+# Compile the model
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# Fit the model to the data
+model.fit(X, y, epochs=100, batch_size=32)  # Adjust the number of epochs and batch_size based on your preference
+
+# Make predictions
+predictions = model.predict(X)
+
+# Print the predictions with variable names
+for i, prediction in enumerate(predictions):
+    print("Prediction for variables", ', '.join(variable_names), ":", prediction)
