2ca340aa0c6542bfabc7ac59058fe72a100a6b63
shamsher7909 <134923810+shamsher7909@users.noreply.github.com>
2023-07-14 12:45:10 +0500

    Update ann5.py

--- ann5.py
+++ ann5.py
@@ -36,6 +36,13 @@ prediction_df[variable_names] = data.iloc[:, :-1]
 #prediction_df.to_json('predictions.json', orient='records')
 
 #prediction_df.to_csv('predictions.csv', index=False)
+
+# Set display options
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+# Print the DataFrame
 print(prediction_df)
+
 
 
